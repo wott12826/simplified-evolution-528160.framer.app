c2649ef2e47ba8a66cc8fd233333d5de8f9631e3
wott12826 <freshplayer430@gmail.com>
2025-06-08 13:45:17 +0300

    first commit

---
+++ sites/5raVDHSj6bRiJX41i5fFDL/chunk-YCYPT4Z7.mjs
@@ -0,0 +1,2 @@
+function a(t,e){return{description:"We builds blockchain infrastructure that automates how businesses run \u2014 securely and at scale",favicon:"https://framerusercontent.com/assets/OhBB1EQZKcpflthbNCBY1sUsoC8.png",robots:"max-image-preview:large",title:"URZA LABS - blockchain software for everyone"}}export{a};
+//# sourceMappingURL=chunk-YCYPT4Z7.mjs.map
